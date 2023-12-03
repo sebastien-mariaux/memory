2ec14297b9fe053951e31193a3ed8428a3ba6583
Sébastien Mariaux <smariaux@regate.io>
2023-12-03 03:14:23 +0100

    Close applications when no memory

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::process::Command;
 use std::thread;
 use std::time::Duration;
 
-const WARNING_THRESHOLD: f64 = 95.0;
+const WARNING_THRESHOLD: f64 = 10.0;
 const ALERT_THRESHOLD: f64 = 5.0;
 const LOOP_DELAY: u64 = 5;
+const REFRESH_MEMORY_DELAY: u64 = 10;
+const APPLICATIONS: [&str; 2] = ["tresorit", "discord"];
 
 fn main() {
     let sys_info = SysInfoWrapper::new();
@@ -23,22 +25,47 @@ fn check_memory(sys_info: &SysInfoWrapper) {
     println!("Percent available memory: {:.2}%", percent_available_memory);
 
     if percent_available_memory < ALERT_THRESHOLD {
-        send_alert();
+        kill_applications();
         return;
     }
 
     if percent_available_memory < WARNING_THRESHOLD {
         send_warning();
-        return;
     }
 }
 
-fn send_alert() {
-    let text = format!(
-        "Available memory is below {}%.\nPlease close some applications.",
-        ALERT_THRESHOLD
-    );
-    send_message("ALERT - VERY LOW MEMORY", &text);
+fn kill_applications() {
+    for application in APPLICATIONS.iter() {
+        kill_application(application);
+        thread::sleep(Duration::from_secs(REFRESH_MEMORY_DELAY));
+
+        let sys_info = SysInfoWrapper::new();
+        let percent_available_memory = get_percent_available_memory(&sys_info);
+        if percent_available_memory > ALERT_THRESHOLD {
+            return;
+        }
+    }
+}
+
+fn kill_application(application: &&str) {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("ps aux | grep {}", application))
+        .output()
+        .expect("failed to execute process");
+    let output_str = String::from_utf8_lossy(&output.stdout);
+
+    output_str.lines().for_each(|line| {
+        let pid = line.split_whitespace().nth(1);
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("kill -9 {}", pid.unwrap()))
+            .output()
+            .expect("failed to execute process");
+    });
+
+    let text = format!("{} has been closed to free some memory", application);
+    send_message("APPLICATION CLOSED", &text);
 }
 
 fn send_warning() {
